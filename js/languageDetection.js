741a78eaeec49c78c82720436377a9958f155dbe
yago <yaguitor@gmail.com>
2025-01-13 18:53:37 +0100

    Added more html structure and some divs css

--- js/languageDetection.js
+++ js/languageDetection.js
@@ -1,11 +1,33 @@
+const translations = {
+  en: {
+    greeting: 'Hi! I\'m Yago Pernas',
+    subTitle: 'Junior Full-Stack Developer'
+  },
+  es: {
+    greeting: 'Hola! Soy Yago Pernas',
+    subTitle: 'Desarrollador Full-Stack Junior'
+  }
+}
+
+
+let lang = 'en';
+
 function detectLanguage() {
   const userLang = navigator.language || navigator.userLanguage;
 
-  if (userLang.startsWith('es')) {
-    document.getElementById("greeting").textContent = "Ups... Llegaste demasiado pronto";
-  } else {
-    document.getElementById("greeting").textContent = "Whoops... You came too early";
-  }
+  if (userLang.startsWith('es'))
+    lang = 'es';
+
+  updateTextContents()
+}
+
+function updateTextContents() {
+  document.querySelectorAll('[data-key]').forEach(element => {
+    const key = element.getAttribute('data-key');
+    if (translations[lang] && translations[lang][key]) {
+      element.textContent = translations[lang][key];
+    }
+  });
 }
 
 window.onload = detectLanguage;
